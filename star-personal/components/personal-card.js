aebde631187d2931582e017f7e2b8475e4e5ebc7
mehmet sait ışık <saitisik@Sait-MacBook-Pro.local>
2023-01-17 00:47:57 +0300

    degisklik yapildi

--- star-personal/components/personal-card.js
+++ star-personal/components/personal-card.js
@@ -1,36 +1,29 @@
+import Image from "next/image";
 import { useState, useEffect } from "react";
 import { Col, Row } from "styled-bootstrap-grid";
 import s from "./personal-card.module.scss";
 const PersonalCard = () => {
   const [people, setPeople] = useState([]);
   useEffect(() => {
-    fetch("https://jsonplaceholder.typicode.com/users")
+    fetch("https://jsonplaceholder.typicode.com/todos")
       .then((response) => response.json())
       .then((json) => setPeople(json));
-    console.log(people);
   }, []);
   return (
     <>
       <Row>
-        {people.map((item, index) => {
+        {people.map((item, index)=>{
           return (
             <Col key={index} md={4}>
               <div className={s.box}>
-                <span className={s.boxName}>{item.name}</span>
-                <div style={{ width: "400px", height: "400px" }}>
-                  <img
-                    width="100%"
-                    height="100%"
+                <span className={s.boxName}>{item.title}</span>
+                <div style={{ width: "100px", height: "40px" }}>
+                 { /*<Image
+                    width={100}
+                    height={100}
                     alt="My Image"
-                    src={
-                      "https://codlart.com/_next/image?url=https%3A%2F%2Fimages.ctfassets.net%2Fftnt8rosh7hi%2FWFOQWNUHWeaJ5xulZ3Wth%2F73bc0d278c05b7ea3f0ae9d227c8b49c%2FIMG_1248.jpg&w=3840&q=75"
-                    }
-                  />
-                </div>
-                <div className={s.boxSub}>
-                  <span className={s.boxSubMail}>{item.email}</span>
-                  <span className={s.boxSubPhone}>{item.phone}</span>
-                  <span className={s.boxSubAddress}>{item.address.city}</span>
+                    src={}
+                  />*/}
                 </div>
               </div>
             </Col>
